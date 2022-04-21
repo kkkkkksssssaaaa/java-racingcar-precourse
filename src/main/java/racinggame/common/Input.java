95f636ac3c2cc56bcc7496eaa3a6eb6cc8e3913a
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-21 22:31:32 +0900

    feat: add input model for received input from console

---
+++ src/main/java/racinggame/common/Input.java
@@ -0,0 +1,21 @@
+package racinggame.common;
+
+import nextstep.utils.Console;
+
+public class Input {
+
+    private final String input;
+
+    private Input() {
+        this.input = Console.readLine();
+    }
+
+    public static Input fromConsole() {
+        return new Input();
+    }
+
+    public String[] split(String regex) {
+        return this.input.split(regex);
+    }
+
+}
