a3e83f9883a45e848328b9f3fb0a052bef15c97c
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-25 17:13:52 +0900

    refactor(CarNames): remove logging for init cars

--- src/main/java/racinggame/car/CarNames.java
+++ src/main/java/racinggame/car/CarNames.java
@@ -25,7 +25,6 @@ public class CarNames {
 
     private Set<CarName> initNames() {
         // TODO refactoring at view model
-        System.out.println("자동차 이름을 입력해주세요.");
         Set<CarName> names = new LinkedHashSet<>();
 
         for (String name
